5198254a63901cebef19aac6b421875942d44f49
Oscar420957 <mo420957@uaeh.edu.mx>
2021-11-11 21:50:13 -0600

    :)

--- src/main/java/com/phonecentral/CentralTelefonica.java
+++ src/main/java/com/phonecentral/CentralTelefonica.java
@@ -18,6 +18,7 @@ public class CentralTelefonica extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
+        System.out.println("Hola");
     }
 }
